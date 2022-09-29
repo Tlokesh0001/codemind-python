22f2fbe75364260bc18382f9900b4030ad266d4d
Tlokesh0001 <106659982+Tlokesh0001@users.noreply.github.com>
2022-09-29 20:34:04 +0530

    n = int(input()) if n == 0 : print(0) elif n%9 == 0 : print(9) else : print(n%9)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,8 +1,4 @@
-def add(n,s):
-    if (n == 0 and s<10):
-        return s
-    if (s>9 and n ==0):
-        return add(s,0)
-    return add(n//10,s+n%10)
-x = int(input())
-print(add(x,0))
+n = int(input())
+if n == 0 : print(0)
+elif n%9 == 0 : print(9)
+else : print(n%9)
